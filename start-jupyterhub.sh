80d7d882f5e12433b899b69e07f2f9940938ca18
Siddappa Sandur <siddappa@prodevans.com>
2018-12-05 15:44:27 +0530

    Update start-jupyterhub.sh

--- start-jupyterhub.sh
+++ start-jupyterhub.sh
@@ -10,9 +10,4 @@ PATH=$PATH:/opt/app-root/scripts
 
 exec jupyterhub -f /opt/app-root/etc/jupyterhub_config.py
 
-sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/jupyterhub/app.py
-sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/traitlets/config/application.py
-sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/traitlets/config/loader.py
-sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/ipython_genutils/py3compat.py
-sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/openshift/client/apis/oapi_api.py
 
