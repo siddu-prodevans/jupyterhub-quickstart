22e82e76ca6c91955f2a7dafbf4865d13679322d
Siddappa Sandur <siddappa@prodevans.com>
2018-12-05 13:11:04 +0530

    added sed

--- start-jupyterhub.sh
+++ start-jupyterhub.sh
@@ -9,3 +9,10 @@ PATH=$PATH:/opt/app-root/scripts
 # Start the JupyterHub instance.
 
 exec jupyterhub -f /opt/app-root/etc/jupyterhub_config.py
+
+sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/jupyterhub/app.py
+sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/traitlets/config/application.py
+sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/traitlets/config/loader.py
+sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/ipython_genutils/py3compat.py
+sed -i 's/async/async_req/g' /opt/app-root/lib/python3.5/site-packages/openshift/client/apis/oapi_api.py
+
